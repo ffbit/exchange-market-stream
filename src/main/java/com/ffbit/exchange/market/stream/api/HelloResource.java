679c6eb152ba25e4336b746b2ebe20aa76063a66
Dmytro Chyzhykov <dmytro.chyzhykov@yandex.ru>
2015-08-04 22:02:24 +0200

    Introduced Spring Framework.

--- src/main/java/com/ffbit/exchange/market/stream/api/HelloResource.java
+++ src/main/java/com/ffbit/exchange/market/stream/api/HelloResource.java
@@ -1,14 +1,22 @@
 package com.ffbit.exchange.market.stream.api;
 
+import com.ffbit.exchange.market.stream.service.HelloService;
+import org.springframework.stereotype.Component;
+
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 
 @Path("hello")
+@Component
 public class HelloResource {
+
+    @Inject
+    private HelloService helloService;
 
     @GET
     public String hello() {
-        return "Hello!";
+        return helloService.hello();
     }
 
 }
---
+++ src/main/java/com/ffbit/exchange/market/stream/service/HelloService.java
@@ -0,0 +1,12 @@
+package com.ffbit.exchange.market.stream.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class HelloService {
+
+    public String hello() {
+        return "Hello!";
+    }
+
+}
